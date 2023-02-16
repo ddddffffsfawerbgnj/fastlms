5c5f68cb98ffd5e56fef2086e12d09d320baf1fb
LEE <dmstn356@naver.com>
2023-02-16 12:48:30 +0900

    fastlms 초기화 설정

---
+++ src/main/java/com/zerobase/fastlms/FastLmsApplication.java
@@ -0,0 +1,12 @@
+package com.zerobase.fastlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastLmsApplication.class, args);
+    }
+}
